aba3d081dd8e1c489087e2d9fe9ec02a20233df3
yehboiilance <lancebotha0705@gmail.com>
2024-03-06 16:14:03 +0200

    Changes made at campus on Wednesday

---
+++ NineSoldiers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+import org.w3c.dom.Node;
+
+import java.io.*;
+
+public class NineSoldiers{
+    
+    public static void main(String[] args){
+
+
+        
+
+
+        List<Node> solution = bestFirstSearch(startState);
+
+
+
+    
+    }
+}
---
+++ State.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+
+public class State implements Comparable<State>
+{
+    
+    private static int[] board = new int[14];
+    private int hValue;
+
+    public State(int[] board)
+    {
+        // DO SOMETHING???
+    }
+
+    @Override
+    public int compareTo(State o)
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    }
+
+    public int get_hVlaue()
+    {
+        return hValue;
+    }
+
+    public void setH(int new_hValue){
+        
+        this.hValue = new_hValue;
+    }
+
+    
+    public int[] readStartStateFromFile(String filename){
+        try
+        {
+            String filePath = "C:/Users/lboth/Desktop/inputB.txt/";
+            File file = new File(filePath);
+            Scanner scan = new Scanner(file);
+
+            // Read the file line by line
+            while (scan.hasNextLine())
+            {
+                for(int i = 1; i <=14; i++){
+                String line = scan.nextLine();
+                int number = Integer.parseInt(line);
+                board[i] = number;
+                }
+                
+                
+            }
+            scan.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (NumberFormatException s) {
+            System.out.println("Invalid number format: " + s.getMessage());
+        }
+        return board;
+    }
+}
